85f73bf96edec610054389ba335b006f816057c9
parshurambagade <parshurambagade855@gmail.com>
2026-01-31 14:01:37 +0530

    add: configurations for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import path from "path";
 
 const app = express();
-
-const PORT = 3000;
+const __dirname = path.resolve();
 
 app.get("/api/hello", (req, res) => {
   res.status(200).json({ message: "Welcome to the ecommers app!" });
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on the port: ", PORT);
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on the port: ", ENV.PORT);
 });
